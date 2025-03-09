251ac04f28966354aa58c891d130e405a1ae54f4
PyroWilDx <pyrowildx314.kqm@gmail.com>
2025-03-09 21:51:18 +0100

    Build Jar File

---
+++ src/main/java/pyrowildx/youtube/mp3/ui/Main.java
@@ -0,0 +1,9 @@
+package pyrowildx.youtube.mp3.ui;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(MainApplication.class, args);
+    }
+}
